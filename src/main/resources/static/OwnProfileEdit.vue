1e6ab1e4a4c9baa64722c872c7cf6979fc33c508
pfistdo1 <pfistdo1@students.zhaw.ch>
2024-07-04 20:05:53 +0200

    removed english

--- src/main/resources/static/OwnProfileEdit.vue
+++ src/main/resources/static/OwnProfileEdit.vue
@@ -6,7 +6,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
           </svg>
-          <h1 style="margin-left: 20px">Profile</h1>
+          <h1 style="margin-left: 20px">MisMatches</h1>
         </div>
       </a>
     </div>
